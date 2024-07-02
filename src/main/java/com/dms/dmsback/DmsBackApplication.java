62d6bc24ea7a31f38c6a19303f1d12b6151fa582
Soong-moo <blueday0214@gmail.com>
2024-07-02 14:40:44 +0900

    chore : project init

---
+++ src/main/java/com/dms/dmsback/DmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.dms.dmsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dms/dmsback/DmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dms.dmsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
